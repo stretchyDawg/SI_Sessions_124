62dc7af85740d120e0558e62898e50c3fd4da93e
Christian <cmmorgado53@gmail.com>
2024-02-10 10:43:15 -0500

    Cleaned up module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,8 +8,6 @@ module si {
     exports si;
     exports si.reversi.model;
     exports si.reversi.view;
-    exports si.tic_tac_toe.model;
-    exports si.tic_tac_toe.view;
     exports si.session_activities.unit04;
     exports si.review.javafx_review;
     exports si.review.javafx_review.containers;
