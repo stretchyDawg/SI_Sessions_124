4de328a9bddacad0232e01ca920789b06b2bb552
Christian <cmmorgado53@gmail.com>
2024-03-29 11:45:53 -0400

    Method referencing example

--- src/main/java/si/review/unit09_review/lambdas/shape/ShapeMain.java
+++ src/main/java/si/review/unit09_review/lambdas/shape/ShapeMain.java
@@ -1,12 +1,15 @@
 package si.review.unit09_review.lambdas.shape;
 
 public class ShapeMain {
+    public static double triangleArea (double side) {
+        return Math.sqrt(3) * Math.pow (side, 2) / 4;
+    }    
+    
     public static void main(String[] args) {
-        int x = 5;
-
         Shape circle = (radius) -> {
            return Math.PI * Math.pow(radius, 2);
         };
+        System.out.println(circle.area(2));
         
         /*
         Things the lambda knows:
@@ -17,9 +20,20 @@ public class ShapeMain {
         Requires a Functional Interface.
         */
         
+        Shape triangle = ShapeMain::triangleArea;
+        System.out.println(triangle.area(2));
+
+        /*
+        Lambdas can also use method referencing (as shown above). 
+        Essentially setting area() to be an already made method.
+
+        Method references require:
+        - The return type to be the same.
+        - The parameter type to be the same.
+        */
+
         
 
 
-        System.out.println(circle.area(x));
     }
 }
