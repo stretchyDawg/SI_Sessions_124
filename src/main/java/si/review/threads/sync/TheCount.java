07404e82f314b90d15386cbe7d9231bffacb5475
Christian <cmmorgado53@gmail.com>
2024-04-12 10:44:35 -0400

    the count!

---
+++ src/main/java/si/review/threads/sync/TheCount.java
@@ -0,0 +1,36 @@
+package si.review.threads.sync;
+
+public class TheCount implements Runnable{
+    private static int[] COUNT = new int[1];
+    private final int id;
+
+    public TheCount(int id){
+        this.id = id;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Counter: " + id + " is starting...");
+        
+        for(int i = 0; i < 100000; i++){
+            synchronized(COUNT){                                // <-- Putting this on the inside of the for loop makes it faster as they are looping through the for loop all at once.
+            // System.out.println(this.id + " looping...");     // <-- Uncomment this to see what's looping
+                COUNT[0]++;
+            }
+        }
+
+        System.out.println("Counter: " + id + " has ended!");
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        for(int i = 0; i < 10; i++){
+            TheCount counter = new TheCount(i);
+            Thread thread = new Thread(counter);
+            thread.start();
+        }
+        
+        Thread.sleep(1000);
+        System.out.println(COUNT[0]);
+
+    }
+}
