4c4303c65b82052b7a082b2307ec97a7c999d135
Christian <cmmorgado53@gmail.com>
2024-04-22 15:16:52 -0400

    Duplexer init

---
+++ src/main/java/si/review/networking_review/duplexer/Duplexer.java
@@ -0,0 +1,34 @@
+package si.review.networking_review.duplexer;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Duplexer {
+    
+    private Socket socket;
+    private Scanner in; // For receiving data
+    private PrintWriter out; // For writing data
+
+    public Duplexer(Socket socket) throws IOException{
+        this.socket = socket;
+        this.in = new Scanner(socket.getInputStream());    
+        this.out = new PrintWriter(socket.getOutputStream());
+    }
+
+    public void send(String message){
+
+    }
+
+    public void receive(String message){
+
+    }
+
+    public void close() throws IOException{
+        this.socket.close();
+        this.in.close();
+        this.out.close();
+    }
+
+}
