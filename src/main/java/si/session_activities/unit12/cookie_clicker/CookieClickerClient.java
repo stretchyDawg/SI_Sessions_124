91e6bd092e3319085f684e52462278c7240e7590
Christian <cmmorgado53@gmail.com>
2024-04-26 14:12:14 -0400

    Infinite input

---
+++ src/main/java/si/session_activities/unit12/cookie_clicker/CookieClickerClient.java
@@ -0,0 +1,49 @@
+package si.session_activities.unit12.cookie_clicker;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+import si.review.networking_review.duplexer.Duplexer;
+
+public class CookieClickerClient {
+    private Duplexer duplexer;
+    private Scanner scanner;
+
+    public CookieClickerClient(String host, int port) throws IOException {
+        Socket socket = new Socket(host, port);
+        this.duplexer = new Duplexer(socket);
+        this.scanner = new Scanner(System.in);
+        System.out.println("Connected to server with port " + port + ".");
+    }
+
+    public void start() {
+        try {
+            String input;
+            while(true){
+                System.out.print(">> ");
+                input = scanner.nextLine();
+                if(input.equals("exit")){
+                    break;
+                }
+                else{
+                    this.duplexer.send(input);
+                }                
+            }
+            try {
+                this.duplexer.close();
+                this.scanner.close();
+            } catch (IOException e) {e.printStackTrace();}
+        } catch (Exception e) {e.printStackTrace();} 
+    }
+
+    public static void main(String[] args) {
+        try {
+            CookieClickerClient client = new CookieClickerClient("localhost", 1234); // Connect to server at localhost:1234
+            client.start();
+        } catch (IOException e) {
+            System.out.println("Unable to connect to server: " + e.getMessage());
+        }
+    }
+}
+
--- src/main/java/si/session_activities/unit12/cookie_clicker/CookieClickerServer.java
+++ src/main/java/si/session_activities/unit12/cookie_clicker/CookieClickerServer.java
@@ -1,11 +1,44 @@
 package si.session_activities.unit12.cookie_clicker;
 
+import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
+
+import si.review.networking_review.duplexer.Duplexer;
 
 public class CookieClickerServer {
     private ServerSocket serverSocket;
+    private Duplexer duplexer;
 
     public CookieClickerServer(int port) throws IOException {
-        serverSocket = new ServerSocket(port);
+        this.serverSocket = new ServerSocket(port);
+        System.out.println("\nServer is listening on port " + port);
+    }
+
+    public void start() {
+        try {
+            System.out.println("Waiting for clients...");
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Client connected.\n");
+            duplexer = new Duplexer(clientSocket);
+            
+            String message = ""; 
+            while(!message.equals("exit")){
+                message = duplexer.read();
+                System.out.println("Received from client: " + message);
+            }
+
+            try {
+                if (duplexer != null) {
+                    duplexer.close();
+                }
+                serverSocket.close();
+            } catch (IOException e) {e.printStackTrace();}
+        } catch (IOException e) {e.printStackTrace();} 
+    }
+
+    public static void main(String[] args) throws IOException {
+        CookieClickerServer server = new CookieClickerServer(1234);
+        server.start();
     }
 }
