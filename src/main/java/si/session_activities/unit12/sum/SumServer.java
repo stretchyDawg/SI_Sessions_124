483fc715c94fd22b9a53abff0bf3f377088fd627
Christian <cmmorgado53@gmail.com>
2024-04-26 15:10:49 -0400

    Sum and Product activities

---
+++ src/main/java/si/session_activities/unit12/sum/SumServer.java
@@ -0,0 +1,48 @@
+package si.session_activities.unit12.sum;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import si.review.networking_review.duplexer.Duplexer;
+
+public class SumServer {
+    private ServerSocket serverSocket;
+    private Duplexer duplexer;
+    private int sum;
+    
+    public SumServer(int port) throws IOException {
+        this.sum = 0;
+        ServerSocket serverSocket = new ServerSocket(port);
+        Socket socket = serverSocket.accept();
+        this.duplexer = new Duplexer(socket);
+        handle();
+
+        serverSocket.close();
+    }
+
+    public void handle() throws IOException{
+        String number = duplexer.read();
+        Integer intNumber = Integer.parseInt(number);
+        while(intNumber >= 0){
+            if(intNumber == 0){
+                duplexer.send(String.valueOf(sum));
+                duplexer.close();
+                serverSocket.close();
+            }
+            this.sum += intNumber;
+            intNumber--;
+        }
+    }
+
+    // public static void main(String[] args) throws IOException {
+    //     System.out.println("Starting server...");
+    //     SumServer sumServer = new SumServer(9999);
+        
+
+    // }
+
+    //this does not work 
+
+    
+}
